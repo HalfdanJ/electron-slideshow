7a74e9f7ebc079d189ebda7cd8cf0d9a993212ce
mathewray-google <mathewray@google.com>
2017-01-06 11:30:39 -0500

    added support for fullscreen on ubuntu

--- main.js
+++ main.js
@@ -13,6 +13,8 @@ function createWindow () {
     // Create the browser window.
     win = new BrowserWindow({width: 1200, height: 600})
 
+    win.setFullScreen(true);
+
     // and load the index.html of the app.
     win.loadURL(`file://${__dirname}/index.html`)
 
